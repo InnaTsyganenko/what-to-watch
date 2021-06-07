6deeb23bbbe8a2cef3d936a34fd73cff803ca76d
Nikulichka <poteryaevainna@gmail.ru>
2021-06-07 16:01:45 +0300

    Add routing

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
-import Main from '../main/main';
-
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import MainScreen from '../main-screen/main-screen';
+import LoginScreen from '../login-screen/login-screen';
+import MyListScreen from '../my-list-screen/my-list-screen';
+import FilmScreen from '../films-screen/films-screen';
+import AddReviewScreen from '../add-review-screen/add-review-screen';
+import PlayerScreen from '../player-screen/player-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 function App(props) {
   const {moviesNames, movieTitle, movieGenre, movieReleaseDate} = props;
 
   return (
-    <Main moviesNames={moviesNames}
-      movieTitle={movieTitle}
-      movieGenre={movieGenre}
-      movieReleaseDate={movieReleaseDate}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainScreen moviesNames={moviesNames}
+            movieTitle={movieTitle}
+            movieGenre={movieGenre}
+            movieReleaseDate={movieReleaseDate}
+          />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <LoginScreen />
+        </Route>
+        <Route exact path={AppRoute.MY_LIST}>
+          <MyListScreen />
+        </Route>
+        <Route exact path={AppRoute.FILM}>
+          <FilmScreen />
+        </Route>
+        <Route exact path={AppRoute.ADD_REVIEW}>
+          <AddReviewScreen />
+        </Route>
+        <Route exact path={AppRoute.PLAYER}>
+          <PlayerScreen />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
