07709349ee34f539a5f58c3fff60cf3566883bde
Nikulichka <poteryaevainna@gmail.ru>
2021-08-02 03:04:57 +0300

    Defence, basic criteria, try 1.1

--- project/src/components/film-card-buttons/film-card-buttons.test.js
+++ project/src/components/film-card-buttons/film-card-buttons.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import {Router, Switch, Route} from 'react-router-dom';
+import {render} from '@testing-library/react';
+import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
 import configureStore from 'redux-mock-store';
 import {Provider} from 'react-redux';
@@ -33,30 +32,5 @@ describe('Component: FilmCardButtons', () => {
     );
 
     expect(container.firstChild).toHaveClass('film-card__buttons');
-  });
-
-  it('when user click "Replay Button" should redirect', () => {
-    const playButtonClickHandle = jest.fn();
-    playButtonClickHandle.mockImplementation(
-      () => history.push(AppRoute.PLAYER),
-    );
-
-    render(
-      <Provider store={store}>
-        <Router history={history}>
-          <Switch>
-            <Route exact path={AppRoute.FILM}>
-              <FilmCardButtons onPlayButtonClick={playButtonClickHandle}/>
-            </Route>
-            <Route exact path={AppRoute.PLAYER}><h1>Mock Player Screen</h1></Route>
-          </Switch>
-        </Router>
-      </Provider>,
-    );
-
-    userEvent.click(screen.getByTestId('button-play'));
-
-    expect(playButtonClickHandle).toBeCalled();
-    expect(screen.getByText(/Mock Player Screen/i)).toBeInTheDocument();
   });
 });
